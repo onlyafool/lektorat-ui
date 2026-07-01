2111c06b270f16f923d3a351d55a0622127f3735
OnlyAfool <onlyafool@gmx.net>
2026-07-01 13:00:36 +0200

    Add Electron + GitHub Actions Desktop Build (Win + Mac)

---
+++ electron/main.cjs
@@ -0,0 +1,50 @@
+const { app, BrowserWindow, shell } = require('electron')
+const path = require('path')
+
+const isDev = !app.isPackaged
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1000,
+    minHeight: 600,
+    title: 'Lektorat',
+    icon: path.join(__dirname, '..', 'public', 'favicon.svg'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.cjs'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  })
+
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:3000')
+    mainWindow.webContents.openDevTools()
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '..', 'dist', 'index.html'))
+  }
+
+  mainWindow.setMenu(null)
+
+  mainWindow.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url)
+    return { action: 'deny' }
+  })
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
